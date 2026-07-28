7fc32b4ae91eed35cce6f5f4f5eb11644adb2446
sutharsan <theepansutharsan1117@gmail.com>
2026-07-28 08:10:53 +0530

    feat(types): add typescript interface video-metric.ts

---
+++ src/types/video-metric.ts
@@ -0,0 +1,10 @@
+export interface VideoMetric {
+  id: number;
+  video_id: number;
+  views: number;
+  likes: number;
+  comments: number;
+  shares: number;
+  engagement_rate: number;
+  recorded_at: string;
+}
